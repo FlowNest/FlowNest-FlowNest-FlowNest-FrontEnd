68748d5d0c1871c7ab41242422c7905e3da20a5a
Eduardo <eduardo_pila@outlook.com>
2024-12-04 15:20:14 -0500

    headerPequeño

--- src/app/chats/page.tsx
+++ src/app/chats/page.tsx
@@ -5,13 +5,14 @@ import { useRouter, useSearchParams } from 'next/navigation';
 import "@/styles/chats.scss";
 import "@/styles/pagina-chats.scss";
 import { PaginaBienvenida } from "@/components/paginaBienvenida";
+import { HeaderPaginas } from "@/components/headerPaginas";
 
 const Chats = () => {
     const router = useRouter();
     return (
         <div className="contenedorPaginaChats">
             <div className="contactos">
-
+                <HeaderPaginas></HeaderPaginas>
             </div>
             <div className="mensajes">
                 <PaginaBienvenida></PaginaBienvenida>
---
+++ src/components/headerPaginas.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const HeaderPaginas = () => {
+  return (
+    <div>H</div>
+  )
+}
